83727a0bdb86616b72a9fb885463b29c22d6d42b
INTERNAL\dmitriy.a <dmitriy.a@tabella.cz>
2019-05-21 10:25:45 +0200

    general setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Layout from './components/Layout';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Hello world</h1>
+      </Layout>
+    );
+  }
 }
 
 export default App;
