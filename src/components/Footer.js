53f7d97fe659e0df42e79bb9d375d5074212e6f4
INTERNAL\dmitriy.a <dmitriy.a@tabella.cz>
2019-05-23 18:20:54 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ const FooterContainer = styled.div`
 
 const CouponsTable = styled.div`
   width: 100%;
-  max-width: 1105px;
+  max-width: 1120px;
   margin: 0 auto;
   display: flex;
   justify-content: flex-start;
@@ -31,13 +31,19 @@ const CouponsTable = styled.div`
   }
 `;
 
-const CouponCard = styled.div`
+const CouponCard = styled.a`
   background: #123f70;
   min-width: 133px;
   width: 133px;
   height: 279px;
   margin: 0 2.5px;
   background: url(${cardBackgroundImage}) center center / cover no-repeat;
+  transition: 0.3s;
+  text-decoration: none;
+  &:hover {
+    transform: translateY(-0.5rem);
+    text-decoration: none;
+  }
 `;
 
 const Text = styled.p`
@@ -45,12 +51,28 @@ const Text = styled.p`
   justify-content: center;
   margin: ${props => (props.margin ? props.margin : '0 0 5px')};
   padding: ${props => (props.padding ? props.padding : '0 20px')};
-  background: ${props => (props.background ? props.background : '')};
   color: ${props => (props.color ? props.color : 'white')};
   font-size: ${props => (props.fontSize ? props.fontSize : '17px')};
   text-align: center;
   text-transform: ${props => (props.capitalize ? 'capitalize' : 'uppercase')};
   line-height: 1;
+`;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px 32px;
+  margin-bottom: 1rem;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  text-transform: ${props => (props.capitalize ? 'capitalize' : 'uppercase')};
+  line-height: 1;
+  background: url('${redLineImage}') center center / 100% no-repeat;
+
+  @media (max-width: 992px) {
+    background: none;
+  }
 `;
 
 const Box = styled.div`
@@ -63,18 +85,10 @@ const Box = styled.div`
 
 const Footer = () => (
   <FooterContainer>
-    <Text
-      fontSize="24px"
-      color="#fff"
-      margin="16px 0 0"
-      padding="10px 32px"
-      background={`url(${redLineImage}) center center / contain no-repeat`}
-    >
-      Don’t want to spin? Find your coupon code below:
-    </Text>
+    <Title>Don’t want to spin? Find your coupon code below:</Title>
     <CouponsTable>
       {sortedBonuses.map(bonus => (
-        <CouponCard>
+        <CouponCard key={bonus.id} href={bonus.url}>
           <Box height="166px">
             <Text fontSize="30px" color="#fcf900" margin="16px 0 0">
               {bonus.bonus}
