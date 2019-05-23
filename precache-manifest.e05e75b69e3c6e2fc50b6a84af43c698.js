84fe3706715989fcf76f276a55753d55bcb31744
INTERNAL\dmitriy.a <dmitriy.a@tabella.cz>
2019-05-23 16:04:16 +0200

    Updates

---
+++ precache-manifest.e05e75b69e3c6e2fc50b6a84af43c698.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2becab6f55026dc32ebd7bcd7cd6aea8",
+    "url": "/spin-the-wheel/index.html"
+  },
+  {
+    "revision": "723e9c97cd1f55fc3f6d",
+    "url": "/spin-the-wheel/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9b97f400f3e13632015a",
+    "url": "/spin-the-wheel/static/css/main.6278db85.chunk.css"
+  },
+  {
+    "revision": "723e9c97cd1f55fc3f6d",
+    "url": "/spin-the-wheel/static/js/2.e8f4dbc7.chunk.js"
+  },
+  {
+    "revision": "9b97f400f3e13632015a",
+    "url": "/spin-the-wheel/static/js/main.77ca24d7.chunk.js"
+  },
+  {
+    "revision": "0e61f5307e49ce552904",
+    "url": "/spin-the-wheel/static/js/runtime~main.29ae645e.js"
+  },
+  {
+    "revision": "fa3635cfe611191713a5fbb533a7b371",
+    "url": "/spin-the-wheel/static/media/200.fa3635cf.png"
+  },
+  {
+    "revision": "db7bd71c48fd243ee69c6f944a644c2b",
+    "url": "/spin-the-wheel/static/media/250.db7bd71c.png"
+  },
+  {
+    "revision": "bd693e4e788d29dcdc65bc6936c5ece7",
+    "url": "/spin-the-wheel/static/media/300.bd693e4e.png"
+  },
+  {
+    "revision": "6427f4854e2b82fc1a133fb763819a5d",
+    "url": "/spin-the-wheel/static/media/350.6427f485.png"
+  },
+  {
+    "revision": "d4a0247732e1285657891626f069c0fa",
+    "url": "/spin-the-wheel/static/media/background.d4a02477.png"
+  },
+  {
+    "revision": "93167a41366952dd8e7466ba7030a373",
+    "url": "/spin-the-wheel/static/media/button-active.93167a41.png"
+  },
+  {
+    "revision": "d3f3ea23ce3e16eb15d6a2b587c611d0",
+    "url": "/spin-the-wheel/static/media/button.d3f3ea23.png"
+  },
+  {
+    "revision": "1173e3600f59a4ff897a9963af8125f1",
+    "url": "/spin-the-wheel/static/media/coins.1173e360.png"
+  },
+  {
+    "revision": "626e1d9d9a6faea8471677174ac41f2a",
+    "url": "/spin-the-wheel/static/media/empress-wu.626e1d9d.png"
+  },
+  {
+    "revision": "9a09e3392b03776275ecfc7989591ffd",
+    "url": "/spin-the-wheel/static/media/fire-dragon.9a09e339.png"
+  },
+  {
+    "revision": "da25c203c98d07d7b2ed1090e61ad642",
+    "url": "/spin-the-wheel/static/media/plentiful-treasure.da25c203.png"
+  },
+  {
+    "revision": "1dad7207eff20f1809d04d875e75f37b",
+    "url": "/spin-the-wheel/static/media/scuba-fishing.1dad7207.png"
+  },
+  {
+    "revision": "9c83e3524f8e69b8f076e250e6e8ba91",
+    "url": "/spin-the-wheel/static/media/title.9c83e352.png"
+  },
+  {
+    "revision": "b710250424a3976f3e098eb758240f84",
+    "url": "/spin-the-wheel/static/media/wheel.b7102504.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spin-the-wheel/precache-manifest.afbe95eb596936af74036fde06564a0b.js"
+  "/spin-the-wheel/precache-manifest.e05e75b69e3c6e2fc50b6a84af43c698.js"
 );
 
 self.addEventListener('message', (event) => {
